b017837f533318dc3a1ffba68326ed5bf930b903
TrueMerc <true_merc@mail.ru>
2020-05-18 20:43:47 +0300

    Изменена схема БД в соответствии с требованиями задания.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,49 @@
-create table CAR (
-  id IDENTITY primary key,
-  brand VARCHAR2(150),
-  model VARCHAR2(200),
-  power DOUBLE,
-  year_of_issue YEAR,
-  assessed_value DEC(20)
+CREATE TABLE CAR (
+    id                IDENTITY PRIMARY KEY,
+    brand             VARCHAR2(150),
+    model             VARCHAR2(200),
+    power             DOUBLE,
+    year_of_issue     YEAR
 );
 
-create table AIRPLANE (
-  id IDENTITY primary key,
-  brand VARCHAR2(150),
-  model VARCHAR2(200),
-  manufacturer VARCHAR2(500),
-  year_of_issue YEAR,
-  fuelCapacity INT,
-  seats INT
+CREATE TABLE AIRPLANE (
+    id                IDENTITY PRIMARY KEY,
+    brand             VARCHAR2(150),
+    model             VARCHAR2(200),
+    manufacturer      VARCHAR2(500),
+    year_of_issue     YEAR,
+    fuelCapacity      INT,
+    seats             INT
+);
+
+CREATE TABLE ASSESSMENT (
+    id                IDENTITY PRIMARY KEY,
+    date_time         TIMESTAMP,
+    value             DEC(20)
+);
+
+CREATE TABLE CARS_ASSESSMENTS(
+    car_id            BIGINT,
+    assessment_id     BIGINT,
+
+    PRIMARY KEY (car_id, assessment_id),
+
+    CONSTRAINT fk_car_id FOREIGN KEY (car_id)
+    REFERENCES CAR (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+    CONSTRAINT  fk_assessment_id FOREIGN KEY (assessment_id)
+    REFERENCES ASSESSMENT (id)
+    ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+CREATE TABLE  PLAINS_ASSESSMENTS(
+    airplane_id       BIGINT,
+    assessment_id     BIGINT,
+
+    PRIMARY KEY (airplane_id, assessment_id),
+
+    CONSTRAINT fk_plane_id FOREIGN KEY (airplane_id)
+    REFERENCES AIRPLANE (id)
+    ON DELETE CASCADE ON UPDATE NO ACTION
 );
